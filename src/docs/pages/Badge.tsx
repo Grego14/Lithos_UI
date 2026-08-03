1810a9db81efed1426842ae20680dbfe1c16c7a9
Gregorio Piñero <gre208981@gmail.com>
2026-08-03 11:51:21 -0400

    Apply corrections

--- src/docs/pages/Badge.tsx
+++ src/docs/pages/Badge.tsx
@@ -22,7 +22,6 @@ export const BadgeVariants = () => {
       <Badge className='mb-4' variant='warning'>Warning</Badge>
       <Badge className='mb-4' variant='error'>Error</Badge>
       <Badge variant='info'>Info</Badge>
-      <Badge color="#FF0033">Custom Color</Badge>
     </div>
   )
 }`
@@ -41,48 +40,17 @@ export const BadgeVariants = () => {
 }`
 
   const customCode = `import { Badge } from '../../components/ui/Badge'
-import { Button } from '../../components/ui/Button'
-import { useRef, useState } from 'react'
 
-export const BadgeCustomColor = () => {
-  const [customColor, setCustomColor] = useState('#FF00FF')
-  const [error, setError] = useState('')
-
-  const inputRef = useRef<null | HTMLInputElement>(null)
-
-  const handleFocus = () => setError('')
-
-  const handleCustomColor = () => {
-    if(!inputRef.current) return
-
-    const value = inputRef.current.value
-
-    if (!isHexColor(value)) {
-      setError('Please specify a valid HEX color. (Example: #FF00FF)')
-      return
-    }
-
-    setCustomColor(value)
-  }
-
+export const CustomizedBadge = () => {
   return (
-    <div className='flex flex-col items-center text-center'>
-      <Badge color={customColor} size='medium'>Custom Color</Badge>
-
-      <div className='mt-4 text-center flex items-center'>
-        <input ref={inputRef} type='text' onFocus={handleFocus} defaultValue={customColor} max={7} min={4} className='p-1.5 text-sm outline-none border-2 border-(--lithos-border) shadow-[2px_2px_0_0_var(--lithos-shadow)] focus:shadow-[4px_4px_0_0_var(--lithos-shadow)] hover:shadow-[4px_4px_0_0_var(--lithos-shadow)] max-w-[7.5rem]' />
-        <Button intent='primary' className='ml-6 text-sm' onClick={handleCustomColor}>Use color</Button>
-      </div>
-
-      {error && (<span className='mt-2 text-xs' style={{ color: colors.error }}>{error}</span>)}
-    </div>
+    <Badge color='#FF0033' size='medium'>Custom Color</Badge>
   )
 }`
 
   const handleFocus = () => setError('')
 
   const handleCustomColor = () => {
-    if(!inputRef.current) return
+    if (!inputRef.current) return
 
     const value = inputRef.current.value
 
