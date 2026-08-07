f7234edf62f48d855891aa13b427353d0f7d5805
IncredibleStand <incrediblestand@gmail.com>
2026-08-07 10:40:44 +0100

    chore: 'open in new tab' is now just 'open'
    chore: arrange links in docs' sidebar

--- src/docs/layout/Sidebar.tsx
+++ src/docs/layout/Sidebar.tsx
@@ -31,14 +31,14 @@ export const Sidebar = () => {
       <div>
         <h3 className="text-xs font-black opacity-50 mb-3 px-4 uppercase">Components</h3>
         <nav className="flex flex-col pl-4 ">
+          <Link to="/docs/accordion" className={getLinkClass('/docs/accordion')}>
+            Accordion
+          </Link>
           <Link to="/docs/alert" className={getLinkClass('/docs/alert')}>
             Alert
           </Link>
           <Link to="/docs/avatar" className={getLinkClass('/docs/avatar')}>
             Avatar
-          </Link>
-          <Link to="/docs/accordion" className={getLinkClass('/docs/accordion')}>
-            Accordion
           </Link>
           <Link to="/docs/badge" className={getLinkClass('/docs/badge')}>
             Badge
