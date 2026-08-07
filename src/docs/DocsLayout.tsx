5376e6707be7ab369db39480d9f27422f1e5c457
madhav9757 <madhavsemwal9@gmail.com>
2026-08-07 10:04:29 +0530

    feat: add breadcrumb component

--- src/docs/DocsLayout.tsx
+++ src/docs/DocsLayout.tsx
@@ -61,9 +61,9 @@ const tocRegistry: Record<string, TOCItem[]> = {
     { id: '#custom-color', label: 'Custom color', level: 2 },
   ],
   '/docs/breadcrumb': [
-    { id: '#examples', label: 'Examples', level: 1 },
-    { id: '#collapsible', label: 'Collapsible', level: 2 },
-    { id: '#icon', label: 'Icon', level: 2 },
+    { id: '#examples', label: 'Breadcrumb Types', level: 1 },
+    { id: '#collapsible', label: '1. Collapsible', level: 2 },
+    { id: '#icon-name', label: '2. Simple Icon + Name', level: 2 },
   ],
   '/docs/avatar': [
     { id: '#examples', label: 'Examples', level: 1 },
