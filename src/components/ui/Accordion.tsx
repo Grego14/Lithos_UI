a716007bd2698f5b484d9c05aeb6168030170189
IncredibleStand <incrediblestand@gmail.com>
2026-08-07 13:29:28 +0100

    chore: update accordion to be mobile responsive

--- src/components/ui/Accordion.tsx
+++ src/components/ui/Accordion.tsx
@@ -83,7 +83,7 @@ export const AccordionGroup = forwardRef<HTMLDivElement, AccordionGroupProps>(
 AccordionGroup.displayName = 'AccordionGroup'
 
 const defaultClasses = {
-  container: 'w-110 min-w-[260px] self-start border-2 border-(--lithos-border) duration-75 ease-out transition-shadow shadow-[2px_2px_0_0_var(--lithos-shadow)]',
+  container: 'w-full self-start border-2 border-(--lithos-border) duration-75 ease-out transition-shadow shadow-[2px_2px_0_0_var(--lithos-shadow)]',
   content: 'p-4 min-h-0 overflow-hidden font-body',
   header: 'justify-between text-lg text-start p-3'
 }
@@ -131,11 +131,11 @@ export const Accordion = forwardRef<HTMLDivElement, AccordionProps>(
               {title}
             </span>
             <div className='ml-2 w-4 min-w-4'>
-              <svg className={`max-w-full h-auto ${iconRotation}`} width="100" height="100" viewBox="0 -0.75 17 17" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="#000000">
+              <svg className={`max-w-full h-auto ${iconRotation}`} width="100" height="100" viewBox="0 -0.75 17 17" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
                 <g>
                   <title>{label}</title>
                   <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-                    <path d="M15.812,9.896 C15.587,9.896 15.361,9.834 15.162,9.699 L8.932,5.543 L2.895,9.74 C2.354,10.099 1.625,9.953 1.266,9.412 C0.905,8.873 1.051,8.142 1.592,7.783 L8.28,3.152 C8.673,2.888 9.188,2.888 9.583,3.15 L16.464,7.74 C17.005,8.099 17.152,8.832 16.792,9.371 C16.564,9.713 16.191,9.896 15.812,9.896 L15.812,9.896 Z" fill="#000000"></path>
+                    <path d="M15.812,9.896 C15.587,9.896 15.361,9.834 15.162,9.699 L8.932,5.543 L2.895,9.74 C2.354,10.099 1.625,9.953 1.266,9.412 C0.905,8.873 1.051,8.142 1.592,7.783 L8.28,3.152 C8.673,2.888 9.188,2.888 9.583,3.15 L16.464,7.74 C17.005,8.099 17.152,8.832 16.792,9.371 C16.564,9.713 16.191,9.896 15.812,9.896 L15.812,9.896 Z" fill="currentColor"></path>
                   </g>
                 </g>
               </svg>
