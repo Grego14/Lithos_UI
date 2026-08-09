001e8e6915a24c807ed5d30f84a266081464a7e9
IncredibleStand <incrediblestand@gmail.com>
2026-08-09 14:42:55 +0100

    test(config): setup vitest infrastructure and refactor global type definitions

--- src/global.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-declare namespace React {
-  namespace JSX {
-    interface IntrinsicElements {
-      ht: import('react').DetailedHTMLProps<import('react').HTMLAttributes<HTMLElement>, HTMLElement>;
-    }
-  }
-}
