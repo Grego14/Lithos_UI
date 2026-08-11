a0c45fd15db52e6fbf398ccd29109f1e03257bae
Gregorio Piñero <gre208981@gmail.com>
2026-08-11 14:59:31 -0400

    feat: add Menu icon

---
+++ src/components/ui/icons/IconMenu.tsx
@@ -0,0 +1,12 @@
+import { FiMenu } from 'react-icons/fi';
+import { type IconProps, iconDefaults } from './IconBase'
+
+export const IconMenu = ({ size = iconDefaults.size, strokeWidth = iconDefaults.strokeWidth, ...props }: IconProps) => {
+  return (
+    <FiMenu
+      size={size}
+      strokeWidth={strokeWidth}
+      {...props}
+    />
+  )
+}
