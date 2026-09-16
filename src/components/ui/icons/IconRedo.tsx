8d319099f9919daff670a2413a97c7ff552a4ab0
madhav9757 <madhavsemwal9@gmail.com>
2026-09-16 17:02:40 +0530

    fix: format icon components

--- src/components/ui/icons/IconRedo.tsx
+++ src/components/ui/icons/IconRedo.tsx
@@ -1,10 +1,6 @@
 import { FiRotateCw } from 'react-icons/fi'
 import { type IconProps, iconDefaults } from './IconBase'
 
-export const IconRedo = ({
-  size = iconDefaults.size,
-  strokeWidth = iconDefaults.strokeWidth,
-  ...props
-}: IconProps) => {
+export const IconRedo = ({ size = iconDefaults.size, strokeWidth = iconDefaults.strokeWidth, ...props }: IconProps) => {
   return <FiRotateCw size={size} strokeWidth={strokeWidth} {...props} />
 }
